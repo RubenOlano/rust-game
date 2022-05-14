2eb09139ffb56c0c4c3db59b882e765bbe5d3987
Ruben Olano <rubendavid2711@gmail.com>
2022-05-13 20:10:50 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,145 @@
+use ggez::event::{KeyCode, KeyMods};
+use ggez::{event, graphics, Context, GameResult};
+use oorandom::Rand32;
+use std::collections::LinkedList;
+
+const GRID_SIZE: (i16, i16) = (30, 20);
+const GRID_CELL_SIZE: (i16, i16) = (32, 32);
+
+const SCREEN_SIZE: (f32, f32) = (
+    GRID_SIZE.0 as f32 * GRID_CELL_SIZE.0 as f32,
+    GRID_SIZE.1 as f32 * GRID_CELL_SIZE.1 as f32,
+);
+
+const DESIRED_FPS: u32 = 8;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct GridPosition {
+    x: i16,
+    y: i16,
+}
+
+impl GridPosition {
+    pub fn new(x: i16, y: i16) -> Self {
+        GridPosition { x, y }
+    }
+
+    pub fn random(rng: &mut Rand32, max_x: i16, max_y: i16) -> Self {
+        (
+            rng.rand_range(0..max_x as u32) as i16,
+            rng.rand_range(0..max_y as u32) as i16,
+        )
+            .into()
+    }
+
+    pub fn new_from_move(pos: GridPosition, dir: Direction) -> Self {
+        match dir {
+            Direction::Up => GridPosition::new(pos.x, (pos.y - 1).rem_euclid(GRID_SIZE.1)),
+            Direction::Down => GridPosition::new(pos.x, (pos.y + 1).rem_euclid(GRID_SIZE.1)),
+            Direction::Left => GridPosition::new((pos.x - 1).rem_euclid(GRID_SIZE.0), pos.y),
+            Direction::Right => GridPosition::new((pos.x + 1).rem_euclid(GRID_SIZE.0), pos.y),
+        }
+    }
+}
+
+impl From<GridPosition> for graphics::Rect {
+    fn from(pos: GridPosition) -> Self {
+        graphics::Rect::new_i32(
+            pos.x as i32 * GRID_CELL_SIZE.0 as i32,
+            pos.y as i32 * GRID_CELL_SIZE.1 as i32,
+            GRID_CELL_SIZE.0 as i32,
+            GRID_CELL_SIZE.1 as i32,
+        )
+    }
+}
+
+impl From<(i16, i16)> for GridPosition {
+    fn from(pos: (i16, i16)) -> Self {
+        GridPosition { x: pos.0, y: pos.1 }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction {
+    pub fn inverse(&self) -> Self {
+        match *self {
+            Direction::Up => Direction::Down,
+            Direction::Down => Direction::Up,
+            Direction::Left => Direction::Right,
+            Direction::Right => Direction::Left,
+        }
+    }
+
+    pub fn from_keycode(key: KeyCode) -> Option<Direction> {
+        match key {
+            KeyCode::Up => Some(Direction::Up),
+            KeyCode::Down => Some(Direction::Down),
+            KeyCode::Left => Some(Direction::Left),
+            KeyCode::Right => Some(Direction::Right),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Segment {
+    pos: GridPosition,
+}
+
+impl Segment {
+    pub fn new(pos: GridPosition) -> Self {
+        Segment { pos }
+    }
+}
+
+struct Food {
+    pos: GridPosition,
+}
+
+impl Food {
+    pub fn new(pos: GridPosition) -> Self {
+        Food { pos }
+    }
+
+    fn draw(&self, ctx: &mut Context) -> GameResult<()> {
+        let color = [0.0, 0.0, 1.0, 1.0].into();
+        let rectangle =
+            graphics::Mesh::new_rectangle(ctx, graphics::DrawMode::fill(), self.pos.into(), color)?;
+        graphics::draw(ctx, &rectangle, (ggez::mint::Point2 { x: 0.0, y: 0.0 },))
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+enum Ate {
+    Itself,
+    Food,
+}
+
+struct Snake {
+    head: Segment,
+    dir: Direction,
+    body: LinkedList<Segment>,
+    ate: Option<Ate>,
+    last_update_dir: Direction,
+    next_dir: Option<Direction>,
+}
+
+impl Snake {
+    pub fn new(head: Segment) -> Self {
+        Snake {
+            head,
+            dir: Direction::Right,
+            body: LinkedList::new(),
+            ate: None,
+            last_update_dir: Direction::Right,
+        }
+    }
+}
+
+fn main() {}
